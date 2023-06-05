f9cf9f7160560e5f4eb9f625c651646be075063a
nc415 <nicolejcalamari@gmail.com>
2023-06-05 15:30:27 -0400

    removed comments

--- scripts/denovoSV_plots.R
+++ scripts/denovoSV_plots.R
@@ -15,6 +15,7 @@ library(grid)
 library(tidyverse)
 library("grid")
 library("ggplotify")
+library(dplyr)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -22,17 +23,11 @@ input_bed <- args[1]
 input_outliers <-args [2]
 input_ped <- args[3]
 out_file <- args[4]
-#known_out_file <- args[5]
-
 
 ped <- fread(input_ped)
 
-
-##De novo CMG calls
-#date <- "20220427"
 denovo <- as.data.frame(read.table(input_bed,header = TRUE, sep="\t",stringsAsFactors=FALSE, quote=""))
 outliers <- as.data.frame(read.table(input_outliers,header = TRUE, sep="\t",stringsAsFactors=FALSE, quote=""))
-
 
 denovo %>%  na.omit()
 denovo<- subset(denovo, chrom!= 'chrom')
@@ -46,7 +41,6 @@ denovo_inv <- subset(denovo, SVTYPE == "INV")
 denovo_cpx <- subset(denovo, SVTYPE == "CPX")
 denovo_ctx <- subset(denovo, SVTYPE == "CTX")
 
-
 ##Get some numbers
 length(unique(denovo$name)) #of unique denovos
 length(unique(denovo$sample)) #number of samples with denovos
@@ -54,27 +48,6 @@ length(unique(denovo$sample)) #number of samples with denovos
 length(unique(outliers$name))
 length(unique(outliers$sample))
 outliers_in_gd <- subset(outliers, select= c(name, in_gd))
-
-
-
-# denovo2 <-subset(denovo, !name %in% subset(denovo, GQ == 1 & ALGORITHMS == "wham")$name)
-#known_denovo <- c("phase2_DEL_chr15_52", "phase2_INS_chr16_366", "phase2_DEL_chr16_3125", "phase2_DEL_chr16_4510",
-                  #"phase2_DEL_chr19_484", "phase2_DEL_chr2_2795", "phase2_DEL_chr5_2228", "phase2_DUP_chr1_1428", "phase2_DEL_chr1_3303",
-                  #"phase2_DEL_chr11_64", "phase2_INV_chr11_4", "phase2_DUP_chr12_4348", "phase2_DEL_chr16_590", 
-                  #"phase2_DEL_chr16_3432", "phase2_DEL_chr17_1588", "phase2_DEL_chr19_2844", "phase2_DEL_chr2_8361", "phase2_DEL_chr3_593",
-                  #"phase2_CTX_chr5_1", "phase2_DEL_chr9_1208", "phase2_DUP_chr9_4575", "phase2_DUP_chrX_1004")
-#phase2_DEL_chr22_384 not denovo, not trio
-
-#known_denovos <- subset(denovo, name == "phase2_INS_chr16_366" | name == "phase2_DEL_chr16_3125" | name == "phase2_DEL_chr16_4510" | name == "phase2_DEL_chr16_590" | name == "phase2_DEL_chr16_3432", select= c(name, in_gd, AF))
-
-#known_denovos_in_outliers <- subset(outliers, name == "phase2_INS_chr16_366" | name == "phase2_DEL_chr16_3125" | name == "phase2_DEL_chr16_4510" | name == "phase2_DEL_chr16_590" | name == "phase2_DEL_chr16_3432", select= c(name, in_gd, AF))
-
-
-#table(known_denovo %in% denovo$name)
-#table(known_denovo %in% outliers$name)
-#known_denovo[!known_denovo %in% denovo$name] #gives the known denovos that we do not find in this data
-
-library(dplyr)
 
 denovo %>%
   # subset(chrom != "chrX") %>%
@@ -85,10 +58,6 @@ denovo %>%
 sample_count %>%
   ggplot(aes(y = n)) +
   geom_boxplot() -> boxplot
-
-
-#exclude tmporarily chrX
-# denovo <- subset(denovo, chrom != "chrX")
 
 ##Define colors
 del_col <- "#D43925"
@@ -125,7 +94,6 @@ p_type_count
 
 ggsave("per_type.png", p_type_count, width = 20, height = 15)
 
-
 ##Count by chromosome
 denovo %>%
   select(chrom, name, SVTYPE) %>%
@@ -149,32 +117,11 @@ p_chr_count
 
 ggsave("per_chrom.png", p_chr_count, width = 30, height = 15)
 
-
-# #Count by sample
-# denovo %>%
-#   select(sample, name, SVTYPE) %>%
-#   unique() %>%
-#   ggplot(aes(x = sample, fill = SVTYPE)) +
-#   geom_bar() +
-#   labs(x="Sample", y="Number de novo SVs", title= "Number of De Novo SVs per Sample") +
-#   scale_fill_manual(values = rev(c(del_col, dup_col, ins_col, inv_col, cpx_col, ctx_col))) + 
-#   theme_classic() +
-#   theme(
-#     legend.position = "right",
-#     axis.text = element_text(size = 10),
-#     axis.title = element_text(size = 11),
-#     axis.text.x = element_text(angle = 45, hjust = 1),
-#     panel.border = element_blank(),
-#     axis.line = element_line(colour = "black")) -> p_sample_count
-# p_sample_count
-
-#count by sample
-
+##Count by sample
 df <- as.data.frame(table(denovo$sample))
 df$samples <- as.character(df$Var1)
 df$num_of_denovos <- as.numeric(as.character(df$Freq))
 df$samples <- as.factor(df$samples)
-
 
 p <- ggplot(df, aes(y=num_of_denovos)) + geom_boxplot() + labs(title = "Number of de novo SVs per sample", y = "Number of de novos", x = "Samples") + theme_classic() +
   theme(
@@ -220,18 +167,8 @@ denovo %>%
     legend.text = element_text(size=25)) -> p_af_count
 ggsave("per_freq.png", p_af_count, width = 15, height = 20)
 
-
 denovo_in_gd <- subset(denovo, in_gd == "True")
 denovo_not_in_gd <- subset(denovo, in_gd == "False")
-
-
-subset(denovo_in_gd, AF_interv == "AF 0.001-0.01")$chrom_type_sample
-
-
-denovo_in_gd_sub <- subset(denovo_in_gd, select= c(name,AF_interv))
-
-#denovo_not_in_gd_sub <- subset(denovo_not_in_gd, select= c(name,AF,gnomAD_V2_AF))
-
 
 denovo_in_gd %>%
   select(AF_interv, name, SVTYPE) %>%
@@ -275,20 +212,11 @@ denovo_not_in_gd %>%
     legend.text = element_text(size=25)) -> p_af_count_not_in_gd
 ggsave("per_freq_not_gd.png", p_af_count_not_in_gd, width = 15, height = 20) 
 
-# 
-# lay <- rbind(c(1,1,2,2,3,3))
-# ml <- grid.arrange(p_af_count, p_af_count_in_gd, p_af_count_not_in_gd, layout_matrix = lay, top=textGrob("Chr16 AF Count CMG DeNovo SV Data"))
-# ggsave("chr16.p_af_count.pdf", ml, width = 9, height = 5)
-# 
-
-
-
 ##De novo count by size
 denovo$SVLEN <- as.numeric(denovo$SVLEN)  
 #denovo[denovo$SVLEN < 1,]$SVLEN <- 1
 denovo$SVLEN_interv <- cut(denovo$SVLEN, breaks = c(0, 100, 500, 2500, 10000, 50000, max(denovo$SVLEN)),
                            labels = c("<100bp", "101-500bp", "501bp-2.5kb", "2.5kb-10kb", "10kb-50kb", ">50kb"))
-
 denovo %>%
   select(SVLEN_interv, name, SVTYPE) %>%
   unique() %>%
@@ -309,11 +237,10 @@ denovo %>%
     legend.title = element_text(size=30),
     legend.text = element_text(size=25)) -> p_size_count
 ggsave("size.png", p_size_count, width = 25, height = 20) 
-##Evidence plot
 
+##Evidence plot
 SR_denovo <- subset(denovo, EVIDENCE_FIX == "SR", select = c(ALGORITHMS, GQ))
 caller_quality_denovos <- subset(denovo, select = c(name,ALGORITHMS, GQ, EVIDENCE))
-
 
 denovo %>%
   select(name, SVTYPE, EVIDENCE_FIX) %>%
@@ -372,9 +299,6 @@ denovo_cons <- denovo[,keep_cols]
 ref_cols <- grep("is_", names(denovo_cons))
 denovo_cons_type <- cbind(denovo_cons[,'SVTYPE', drop = FALSE], (denovo_cons[,ref_cols])*1)
 
-# denovo_cons_coding <- subset(denovo_cons, is_coding == 1)
-# denovo_cons_non_coding <- subset(denovo_cons, is_coding == 0)
-
 sv_cols <- list(
   list(query = elements, 
        params = list("SVTYPE", c("DEL","DUP", "INS", "INV", "CPX", "CTX")), color = del_col, active = T
@@ -401,7 +325,6 @@ sv_cols <- list(
   #query.name = "CTX"
   #)
 )
-
 
 denovo_cons_type %>% 
   upset(sets = grep("is_", names(denovo_cons_type), value = T),
@@ -507,10 +430,4 @@ lay <- rbind(c(1,1,2,2,2,2,2,2),
 ml <- grid.arrange(p, p_type_count, p_chr_count, p_af_count, p_af_count_in_gd, p_af_count_not_in_gd, p_size_count, p_evidence, grob_annotation_upset_plot, layout_matrix = lay, top=textGrob("De Novo SV Data", gp=gpar(fontsize=50)))
 ggsave(out_file, ml, width = 70, height = 65, limitsize = FALSE)
 
-
-# 
-# 
-# lay <- rbind(c(1,1,2,2,3,3))
-# ml <- grid.arrange(p_af_count_known, p_size_count_known, p_evidence_known, layout_matrix = lay, top=textGrob("Chr16 Known CMG De Novo SV Data"))
-# ggsave(known_out_file, ml, width = 8, height = 5)
 
