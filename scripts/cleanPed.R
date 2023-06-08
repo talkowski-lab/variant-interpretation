8574e4d9477ecefa0322ea22cc0ad58c1b77ee1e
nc415 <nicolejcalamari@gmail.com>
2023-06-08 12:50:57 -0400

    changes

--- scripts/cleanPed.R
+++ scripts/cleanPed.R
@@ -57,18 +57,35 @@ split_fam<-function(probands,pedigree){
   return(trio)
 }
 
-trios<-split_fam(large_families_prob, large_families)
+output<-split_fam(large_families_prob, large_families)
 
 #subset out newFamID column and rename FamID
-new_trios <- subset(trios, select=c("newFamID", "IndividualID", "MotherID", "FatherID", "Gender", "Affected"))
-colnames(new_trios)[1] <- "FamID"
+new_output <- subset(output, select=c("newFamID", "IndividualID", "MotherID", "FatherID", "Gender", "Affected"))
+colnames(new_output)[1] <- "FamID"
+
+#subset to just families with all three in the new ped
+new_output %>%
+  group_by(FamID) %>%
+  mutate(FamSize = n()) %>%
+  filter(FamSize==3)-> trios
+
+new_trios <- subset(trios, select=c("FamID", "IndividualID", "MotherID", "FatherID", "Gender", "Affected"))
+
+#If mom and dad are not in original ped file as an individual, remove them so that they do not cause an error in the de novo script
+new_trios$FatherID[!(new_trios$FatherID %in% ped$IndividualID)] <- 0
+new_trios$MotherID[!(new_trios$MotherID %in% ped$IndividualID)] <- 0
 
 #subset ped to only include families that are not > 3
 ped_without_large_families <- subset(ped, !(FamID %in% large_family_ids))
 
+#If mom and dad are not in original ped file as an individual, remove them so that they do not cause an error in the de novo script
+ped_without_large_families$FatherID[!(ped_without_large_families$FatherID %in% ped$IndividualID)] <- 0
+ped_without_large_families$MotherID[!(ped_without_large_families$MotherID %in% ped$IndividualID)] <- 0
+
 #concat new trio pedigree and old pedigree without families > 3
 ped_without_large_families_df <- data.frame(ped_without_large_families)
 new_trios_df <- data.frame(new_trios)
+
 final_ped<-rbind(new_trios_df, ped_without_large_families_df)
 
 write.table(final_ped,file="cleaned_ped.txt",quote=F,row.names=F,col.names=T,sep="\t")
