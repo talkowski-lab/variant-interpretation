21f0474af7b1ebdf4d62e560da199c734b74b7a7
nc415 <nicolejcalamari@gmail.com>
2023-06-30 12:26:22 -0400

    updated localization with updated cram names

--- scripts/renameCramsLocalize.py
+++ scripts/renameCramsLocalize.py
@@ -15,28 +15,30 @@ args = parser.parse_args()
 ped_file = args.ped
 sample_cc = args.scc
 
-
+'''
 def mv(file, output):
 	#process1 = Popen(['gcloud', 'auth', 'application-default', 'print-access-token'], stdout=PIPE)
 	#os.environ['GCS_OAUTH_TOKEN'] = process1.stdout.read().decode()
 	result = subprocess.run(["mv", file, output])
 	print(result.stderr)
+'''
 
 #find number of trios in ped file
 ped=pd.read_csv(ped_file,sep='\t')
 colnames = colnames=['sample', 'crai', 'cram']
-crams=pd.read_csv(sample_cc,sep='\t',names=colnames)
+cram=pd.read_csv(sample_cc,sep='\t',names=colnames)
 pd.options.display.max_colwidth = 500
 
 #change to cromwell root so they can be moved
-crams['cram'] = crams['cram'].str.replace('gs://', '/cromwell_root/')
-crams['crai'] = crams['crai'].str.replace('gs://', '/cromwell_root/')
+cram['cram'] = cram['cram'].str.replace('gs://', '/cromwell_root/')
+cram['crai'] = cram['crai'].str.replace('gs://', '/cromwell_root/')
 
+'''
 cram = crams
 for col in crams.columns:
 	cram[col] = crams[col].apply(lambda x: x.strip())
 print(cram['cram'])
-
+'''
 maternal_list = ped["MotherID"]
 paternal_list = ped["FatherID"]
 sample_list = ped["IndividualID"]
@@ -49,10 +51,10 @@ cram_file_mom_suffix = cram_file_mom.split('/')[-1]
 crai_file_mom_suffix = crai_file_mom.split('/')[-1]
 cram_file_mom_prefix = cram_file_mom.split(cram_file_mom_suffix)[0]
 crai_file_mom_prefix = crai_file_mom.split(crai_file_mom_suffix)[0]
-mv(cram_file_mom, cram_file_mom_prefix + 'MOTHER.' + str(cram_file_mom_suffix))
-mv(crai_file_mom, crai_file_mom_prefix +  'MOTHER.' + str(crai_file_mom_suffix))
-cram.loc[cram['sample'].isin(maternal_list), 'new_cram'] = cram_file_mom_prefix + 'MOTHER.' + str(cram_file_mom_suffix)
-cram.loc[cram['sample'].isin(maternal_list), 'new_crai'] = crai_file_mom_prefix + 'MOTHER.' + str(crai_file_mom_suffix)
+#mv(cram_file_mom, cram_file_mom_prefix + 'MOTHER.' + str(cram_file_mom_suffix))
+#mv(crai_file_mom, crai_file_mom_prefix +  'MOTHER.' + str(crai_file_mom_suffix))
+cram.loc[cram['sample'].isin(maternal_list), 'new_cram'] = 'MOTHER.' + str(cram_file_mom_suffix)
+cram.loc[cram['sample'].isin(maternal_list), 'new_crai'] = 'MOTHER.' + str(crai_file_mom_suffix)
 
 
 paternal_list = [i for i in paternal_list if i != '0']
@@ -63,10 +65,10 @@ cram_file_dad_suffix = cram_file_dad.split('/')[-1]
 crai_file_dad_suffix = crai_file_dad.split('/')[-1]
 cram_file_dad_prefix = cram_file_dad.split(cram_file_dad_suffix)[0]
 crai_file_dad_prefix = crai_file_dad.split(crai_file_dad_suffix)[0]
-mv(cram_file_dad, cram_file_dad_prefix + 'FATHER.' + str(cram_file_dad_suffix))
-mv(crai_file_dad, crai_file_dad_prefix + 'FATHER.' + str(crai_file_dad_suffix))
-cram.loc[cram['sample'].isin(paternal_list), 'new_cram'] = cram_file_dad_prefix + 'FATHER.' + str(cram_file_dad_suffix)
-cram.loc[cram['sample'].isin(paternal_list), 'new_crai'] = crai_file_dad_prefix + 'FATHER.' + str(crai_file_dad_suffix)
+#mv(cram_file_dad, cram_file_dad_prefix + 'FATHER.' + str(cram_file_dad_suffix))
+#mv(crai_file_dad, crai_file_dad_prefix + 'FATHER.' + str(crai_file_dad_suffix))
+cram.loc[cram['sample'].isin(paternal_list), 'new_cram'] = 'FATHER.' + str(cram_file_dad_suffix)
+cram.loc[cram['sample'].isin(paternal_list), 'new_crai'] = 'FATHER.' + str(crai_file_dad_suffix)
 
 
 proband_list = [i for i in sample_list if (i not in maternal_list) & (i not in paternal_list)]
@@ -90,10 +92,10 @@ cram_file_p_suffix = cram_file_p.split('/')[-1]
 crai_file_p_suffix = crai_file_p.split('/')[-1]
 cram_file_p_prefix = cram_file_p.split(cram_file_p_suffix)[0]
 crai_file_p_prefix = crai_file_p.split(crai_file_p_suffix)[0]
-mv(cram_file_p, cram_file_p_prefix + 'PROBAND.' + str(cram_file_p_suffix))
-mv(crai_file_p, crai_file_p_prefix + 'PROBAND.' + str(crai_file_p_suffix))
-cram.loc[cram['sample'].isin(affected_list_proband), 'new_cram'] = cram_file_p_prefix + 'PROBAND.' + str(cram_file_p_suffix)
-cram.loc[cram['sample'].isin(affected_list_proband), 'new_crai'] = crai_file_p_prefix + 'PROBAND.' + str(crai_file_p_suffix)
+#mv(cram_file_p, cram_file_p_prefix + 'PROBAND.' + str(cram_file_p_suffix))
+#mv(crai_file_p, crai_file_p_prefix + 'PROBAND.' + str(crai_file_p_suffix))
+cram.loc[cram['sample'].isin(affected_list_proband), 'new_cram'] = 'PROBAND.' + str(cram_file_p_suffix)
+cram.loc[cram['sample'].isin(affected_list_proband), 'new_crai'] = 'PROBAND.' + str(crai_file_p_suffix)
 
 
 sibling = proband[proband['Affected'] == 1]
@@ -105,10 +107,10 @@ cram_file_sib_suffix = cram_file_sib.split('/')[-1]
 crai_file_sib_suffix = crai_file_sib.split('/')[-1]
 cram_file_sib_prefix = cram_file_sib.split(cram_file_sib_suffix)[0]
 crai_file_sib_prefix = crai_file_sib.split(crai_file_sib_suffix)[0]
-mv(cram_file_sib, cram_file_sib_prefix + 'SIBLING.' + str(cram_file_sib_suffix))
-mv(crai_file_sib, cram_file_sib_prefix + 'SIBLING.' + str(crai_file_sib_suffix))
-cram.loc[cram['sample'].isin(sibling_list), 'new_cram'] = cram_file_sib_prefix + 'SIBLING.' + str(cram_file_sib_suffix)
-cram.loc[cram['sample'].isin(sibling_list), 'new_crai'] = cram_file_sib_prefix + 'SIBLING.' + str(crai_file_sib_suffix)
+#mv(cram_file_sib, cram_file_sib_prefix + 'SIBLING.' + str(cram_file_sib_suffix))
+#mv(crai_file_sib, cram_file_sib_prefix + 'SIBLING.' + str(crai_file_sib_suffix))
+cram.loc[cram['sample'].isin(sibling_list), 'new_cram'] = 'SIBLING.' + str(cram_file_sib_suffix)
+cram.loc[cram['sample'].isin(sibling_list), 'new_crai'] = 'SIBLING.' + str(crai_file_sib_suffix)
 
 
 mat = {
