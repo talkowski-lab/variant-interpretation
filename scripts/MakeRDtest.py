37af2ea972fc78dab9a204af86f9e5059eab39e6
alsanju <alba.sanchis91@gmail.com>
2023-08-08 17:43:44 -0400

    Create file

--- scripts/MakeRDtest.py
+++ scripts/MakeRDtest.py
@@ -20,7 +20,7 @@ def vconcat_resize(img_list, interpolation
                       (w_min, int(img.shape[0] * w_min / img.shape[1])),
                                  interpolation = interpolation)
                       for img in img_list]
-    # return final image
+    # return final imageimg_v_resize
     return cv2.vconcat(im_list_resize)
   
 # combine two images side by side
