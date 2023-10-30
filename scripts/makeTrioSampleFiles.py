271d095534b11ea03272ef054c561952e8e299fc
Eren Shin <eshin0525@gmail.com>
2023-10-30 11:20:07 -0400

    updated pedigree file

--- scripts/makeTrioSampleFiles.py
+++ scripts/makeTrioSampleFiles.py
@@ -10,6 +10,12 @@ storage_client = storage.Client(project='talkowski-sv-gnomad')
 bucket = storage_client.get_bucket(ped_uri.split('/')[2])
 
 ped = pd.read_csv(ped_uri, sep='\t')
+ped['IndividualID'] = ped['IndividualID'].str.split('__').str[1].str.upper()
+ped['FatherID'] = ped['FatherID'].str.split('__').str[1].str.upper().fillna(0)
+ped['MotherID'] = ped['MotherID'].str.split('__').str[1].str.upper().fillna(0)
+bucket.blob(f"resources/metadata/{ped_uri.split('/')[-1]}").upload_from_string(ped.to_csv(sep='\t', index=False), 'text/csv')
+print('Updated pedigree csv saved')
+
 trio = ped[(ped.FatherID != '0') & (ped.MotherID != '0')].iloc[:, :4]
 trio['TrioID'] = trio['FamID'] + '-' + trio['IndividualID']
 trio.rename(columns={'IndividualID': 'SampleID'}, inplace=True)
