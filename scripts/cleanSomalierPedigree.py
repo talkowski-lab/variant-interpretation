c047ef444f2edc17844f159e8f4cc4fcef972984
Eren Shin <eshin0525@gmail.com>
2023-12-15 12:57:22 -0500

    changed header for getPVR

--- scripts/cleanSomalierPedigree.py
+++ scripts/cleanSomalierPedigree.py
@@ -31,13 +31,15 @@ somalier = somalier.replace({'-9': '0'})
 new_ped = ped.copy()
 
 new_ped['sex_error'] = somalier.sex!=ped.sex
+new_ped['family_error'] = somalier.family_id!=ped.family_id
 new_ped['maternal_error'] = somalier.maternal_id!=ped.maternal_id
 new_ped['paternal_error'] = somalier.paternal_id!=ped.paternal_id
 
-ped[~(new_ped.sex_error | new_ped.maternal_error | new_ped.paternal_error)].reset_index(drop=True).sort_values('sample_id').to_csv(f"{cohort_prefix}_ped_corrected.ped", sep='\t', index=False)
+ped[~(new_ped.sex_error | new_ped.family_error | new_ped.maternal_error | new_ped.paternal_error)].reset_index(drop=True).sort_values('sample_id').to_csv(f"{cohort_prefix}_ped_corrected.ped", sep='\t', index=False)
 
 errors_df = pd.DataFrame({'sex_error':new_ped[new_ped.sex_error].sample_id,
-                         'maternal_error':new_ped[new_ped.maternal_error].sample_id,
-                         'paternal_error':new_ped[new_ped.paternal_error].sample_id}).reset_index(drop=True)
+                        'family_error': new_ped[new_ped.family_error].sample_id,
+                        'maternal_error':new_ped[new_ped.maternal_error].sample_id,
+                        'paternal_error':new_ped[new_ped.paternal_error].sample_id}).reset_index(drop=True)
 
 errors_df.to_csv(f"{cohort_prefix}_somalier_errors.tsv", index=False, sep="\t", na_rep="")
