45fda1196be003716f2ec595aa7614857d266957
Eren Shin <eshin0525@gmail.com>
2023-12-21 07:33:15 -0800

    maximize Hail memory

--- scripts/vep_annotate_hail.py
+++ scripts/vep_annotate_hail.py
@@ -1,8 +1,22 @@
+from pyspark.sql import SparkSession
 import hail as hl
 import sys
 
 vcf_file = sys.argv[1]
 vep_annotated_vcf_name = sys.argv[2]
+cores = sys.argv[3]
+mem = sys.argv[4]
+
+builder = (
+                SparkSession 
+                .builder
+                .config("spark.executor.cores", cores)
+                .config("spark.executor.memory", f"{mem}g")
+                .enableHiveSupport()
+)
+                               
+spark = builder.getOrCreate()
+hl.init(sc=spark.sparkContext)
 
 mt = hl.import_vcf(vcf_file, force_bgz=True, reference_genome='GRCh38')
 mt = hl.vep(mt, config='vep_config.json', csq=True)
