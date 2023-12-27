e4fea9537aa53885251aa2b74387492387e2d0f3
Eren Shin <eshin0525@gmail.com>
2023-12-27 16:02:25 -0500

    filter rare variants in Hail

---
+++ scripts/wgs_ultra_rare_variants_hail.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# adapted from wgs_ultra_rare_variants_hail.ipynb in Terra Analysis
+
+from pyspark.sql import SparkSession
+from google.cloud import storage
+import hail as hl
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import sys
+
+build = 'GRCh38'
+lcr_uri = sys.argv[1]
+ped_uri = sys.argv[2]
+meta_uri = sys.argv[3]
+trio_uri = sys.argv[4]
+vcf_uri = sys.argv[5]
+cohort_prefix = sys.argv[6]
+cores = sys.argv[7]
+mem = sys.argv[8]
+
+hl.init(spark_conf={"spark.executor.cores": cores, 
+                    "spark.executor.memory": f"{mem}g"})
+
+pedigree = hl.Pedigree.read(ped_uri)
+
+trio_df = pd.read_csv(trio_uri, sep='\t')
+
+mt = hl.import_vcf(vcf_file, force_bgz=True, reference_genome='GRCh38')
+
+#split-multi
+def split_multi_ssc(mt):
+    mt = mt.annotate_rows(num_alleles = mt.alleles.size() ) # Add number of alleles at site before split
+    # Now split
+    sm = hl.split_multi(mt)
+    pl = hl.or_missing(hl.is_defined(sm.PL),
+                      (hl.range(0, 3).map(lambda i: hl.min(hl.range(0, hl.len(sm.PL))
+       .filter(lambda j: hl.downcode(hl.unphased_diploid_gt_index_call(j), sm.a_index) == hl.unphased_diploid_gt_index_call(i))
+       .map(lambda j: sm.PL[j])))))
+    split_ds = sm.annotate_entries(GT = hl.downcode(sm.GT, sm.a_index),
+                                   AD = hl.or_missing(hl.is_defined(sm.AD), [hl.sum(sm.AD) - sm.AD[sm.a_index], sm.AD[sm.a_index]]),
+                                   PL = pl) 
+        #GQ = hl.cond(hl.is_defined(pl[0]) & hl.is_defined(pl[1]) & hl.is_defined(pl[2]), hl.gq_from_pl(pl), sm.GQ) )
+    mt = split_ds.drop('old_locus', 'old_alleles')
+    return mt
+
+mt = split_multi_ssc(mt)
+# annotate cohort ac to INFO field (after splitting multiallelic)
+mt = mt.annotate_rows(info=mt.info.annotate(cohort_AC=mt.info.AC[mt.a_index - 1],
+                                           cohort_AF=mt.info.AF[mt.a_index - 1]))
+mt = mt.annotate_rows(ID=hl.variant_str(mt.row_key))
+
+# useful table here: https://hail.is/docs/0.2/methods/genetics.html#hail.methods.transmission_disequilibrium_test
+# t=1 u=0
+
+# try filtering before
+mt_filtered = mt
+mt_filtered = mt_filtered.filter_rows(mt_filtered.info.cohort_AC < 20 , keep=True)
+# filter low complexity regions
+try:
+    lcr = hl.import_bed(lcr_uri, reference_genome=build)
+except Exception as e:
+    lcr = hl.import_bed(lcr_uri, reference_genome=build, force_bgz=True)
+mt_filtered = mt_filtered.filter_rows(hl.is_defined(lcr[mt_filtered.locus]), keep=False)
+# select samples after qc
+meta = hl.import_table(meta_uri, types={'SampleID': hl.tstr, 'FamID': hl.tstr, 'Role': hl.tstr})
+meta = meta.annotate(s=meta.SampleID).key_by('s')
+mt_filtered = mt_filtered.annotate_cols(pheno=meta[mt_filtered.s])
+mt_filtered = mt_filtered.filter_cols(mt_filtered.pheno.Role != '', keep = True)
+
+tdt_table_filtered = hl.transmission_disequilibrium_test(mt_filtered, pedigree)
+tdt_filtered_df = tdt_table_filtered.to_pandas()
+
+mt_filtered_rare = mt_filtered.filter_entries((mt_filtered.info.cohort_AC<=2)&(mt_filtered.info.cohort_AF<=0.05))
+tdt_table_filtered_rare = tdt_table_filtered.semi_join(mt_filtered_rare.rows())
+
+tdt_filtered_rare_df = tdt_table_filtered_rare.to_pandas()
+
+ultra_rare_vars_table = tdt_table_filtered_rare.filter((tdt_table_filtered_rare.t==1) & (tdt_table_filtered_rare.u==0))
+
+trio_mat = hl.trio_matrix(mt_filtered, pedigree).semi_join_rows(ultra_rare_vars_table)
+
+ultra_rare_vars_df = trio_mat.filter_entries(trio_mat.proband_entry.GT.is_het()).entries().to_pandas()
+
+ultra_rare_vars_df = ultra_rare_vars_df[ultra_rare_vars_df['proband.s'].isin(trio_df.SampleID)]
+
+ultra_rare_vars_df['father_entry.VAF'] = ultra_rare_vars_df['father_entry.AD'].str[1] / (ultra_rare_vars_df['father_entry.AD'].str[0]+ultra_rare_vars_df['father_entry.AD'].str[1])
+ultra_rare_vars_df['mother_entry.VAF'] = ultra_rare_vars_df['mother_entry.AD'].str[1] / (ultra_rare_vars_df['mother_entry.AD'].str[0]+ultra_rare_vars_df['mother_entry.AD'].str[1])
+ultra_rare_vars_df['proband_entry.VAF'] = ultra_rare_vars_df['proband_entry.AD'].str[1] / (ultra_rare_vars_df['proband_entry.AD'].str[0]+ultra_rare_vars_df['proband_entry.AD'].str[1])
+
+child_format_fields = ['GT','AD','DP','GQ','PGT','PID','PL','VAF']  #'AB'
+parent_format_fields = ['GT','AD','DP','GQ','VAF']  #'AB'
+rename_cols = {f"mother_entry.{field}": f"{field}_mother" for field in parent_format_fields} |\
+    {f"father_entry.{field}": f"{field}_father" for field in parent_format_fields} |\
+    {f"proband_entry.{field}": f"{field}_sample" for field in child_format_fields} |\
+    {'qual': 'QUAL', 'proband.s': 'SAMPLE'}
+ultra_rare_vars_df = ultra_rare_vars_df.rename(rename_cols, axis=1)
+
+ultra_rare_vars_df['CHROM'] = ultra_rare_vars_df.locus.astype(str).str.split(':').str[0]
+ultra_rare_vars_df['POS'] = ultra_rare_vars_df.locus.astype(str).str.split(':').str[1].astype(int)
+ultra_rare_vars_df['REF'] = ultra_rare_vars_df.alleles.str[0]
+ultra_rare_vars_df['ALT'] = ultra_rare_vars_df.alleles.str[1]
+ultra_rare_vars_df['LEN'] = abs(ultra_rare_vars_df.REF.str.len()-ultra_rare_vars_df.ALT.str.len())
+ultra_rare_vars_df['TYPE'] =np.where(ultra_rare_vars_df.LEN==0, 'SNV', 'Indel')
+
+ultra_rare_vars_df.columns = ultra_rare_vars_df.columns.str.replace('info.', '')
+
+# 'POLYX'
+info_cols = ['END','AC','AF','AN','BaseQRankSum','ClippingRankSum','DP','FS','MLEAC','MLEAF','MQ','MQRankSum','QD','ReadPosRankSum','SOR','VQSLOD','cohort_AC', 'cohort_AF', 'CSQ']
+cols_to_keep = ['CHROM', 'POS', 'REF', 'ALT', 'LEN', 'TYPE', 'ID'] + info_cols + list(rename_cols.values())
+
+ultra_rare_vars_df[cols_to_keep].to_csv(f"{cohort_prefix}_ultra_rare_variants.tsv", sep='\t', index=False)
---
+++ wdl/filterRareVariantsHail.wdl
@@ -0,0 +1,89 @@
+version 1.0
+
+struct RuntimeAttr {
+    Float? mem_gb
+    Int? cpu_cores
+    Int? disk_gb
+    Int? boot_disk_gb
+    Int? preemptible_tries
+    Int? max_retries
+}
+
+workflow filterRareVariantsHail {
+    input {
+        File vcf_file
+        File lcr_uri
+        File ped_uri
+        File meta_uri
+        File trio_uri
+        File filter_rare_variants_python_script
+        String vep_hail_docker
+        String cohort_prefix
+        RuntimeAttr? runtime_attr_filter_vcf
+    }
+
+    call filterRareVariants {
+        input:
+            vcf_file=vcf_file,
+            lcr_uri=lcr_uri,
+            ped_uri=ped_uri,
+            meta_uri=meta_uri,
+            trio_uri=trio_uri,
+            filter_rare_variants_python_script=filter_rare_variants_python_script,
+            vep_hail_docker=vep_hail_docker,
+            cohort_prefix=cohort_prefix
+    }
+
+    output {
+        File ultra_rare_variants_tsv = filterRareVariants.ultra_rare_variants_tsv
+    }
+}
+
+task filterRareVariants {
+    input {
+        File vcf_file
+        File lcr_uri
+        File ped_uri
+        File meta_uri
+        File trio_uri
+        File filter_rare_variants_python_script
+        String vep_hail_docker
+        String cohort_prefix
+        RuntimeAttr? runtime_attr_override
+    }
+
+    Float input_size = size(vcf_file, "GB")
+    Float base_disk_gb = 10.0
+    Float input_disk_scale = 5.0
+    RuntimeAttr runtime_default = object {
+        mem_gb: 4,
+        disk_gb: ceil(base_disk_gb + input_size * input_disk_scale),
+        cpu_cores: 1,
+        preemptible_tries: 3,
+        max_retries: 1,
+        boot_disk_gb: 10
+    }
+
+    RuntimeAttr runtime_override = select_first([runtime_attr_override, runtime_default])
+    Float memory = select_first([runtime_override.mem_gb, runtime_default.mem_gb])
+    Int cpu_cores = select_first([runtime_override.cpu_cores, runtime_default.cpu_cores])
+
+    runtime {
+        memory: "~{memory} GB"
+        disks: "local-disk ~{select_first([runtime_override.disk_gb, runtime_default.disk_gb])} HDD"
+        cpu: cpu_cores
+        preemptible: select_first([runtime_override.preemptible_tries, runtime_default.preemptible_tries])
+        maxRetries: select_first([runtime_override.max_retries, runtime_default.max_retries])
+        docker: vep_hail_docker
+        bootDiskSizeGb: select_first([runtime_override.boot_disk_gb, runtime_default.boot_disk_gb])
+    }
+
+    command {
+        python 3.9 ~{filter_rare_variants_python_script} ~{lcr_uri} ~{ped_uri} ~{meta_uri} ~{trio_uri} ~{vcf_file} \
+        ~{cohort_prefix} ~{cpu_cores} ~{memory}
+    }
+
+    output {
+        File ultra_rare_variants_tsv = cohort_prefix + '_ultra_rare_variants.tsv'
+    }
+}
