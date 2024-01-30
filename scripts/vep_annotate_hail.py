258ab4c8f9880d9cb3e90c50f5a40043031c8b3d
Eren Shin <eshin0525@gmail.com>
2024-01-30 13:29:22 -0500

    array_elements_required=False

--- scripts/vep_annotate_hail.py
+++ scripts/vep_annotate_hail.py
@@ -31,7 +31,7 @@ def split_multi_ssc(mt):
     return mt
 
 header = hl.get_vcf_metadata(vcf_file) 
-mt = hl.import_vcf(vcf_file, force_bgz=True, reference_genome='GRCh38')
+mt = hl.import_vcf(vcf_file, force_bgz=True, array_elements_required=False, reference_genome='GRCh38')
 mt = split_multi_ssc(mt)
 # annotate cohort ac to INFO field (after splitting multiallelic)
 mt = mt.annotate_rows(info=mt.info.annotate(cohort_AC=mt.info.AC[mt.a_index - 1],
