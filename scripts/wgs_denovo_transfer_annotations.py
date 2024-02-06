a49d0a2344d4a044a7b8ed544508751260e164c0
Eren Shin <eshin0525@gmail.com>
2024-02-05 22:10:10 -0500

    CHROM

--- scripts/wgs_denovo_transfer_annotations.py
+++ scripts/wgs_denovo_transfer_annotations.py
@@ -16,7 +16,7 @@ merged_split_trio_df = pd.read_csv(merged_split_trio_tsv, sep='\t')
 merged_split_trio_df['VarKey'] = merged_split_trio_df.locus.astype(str) + ':' + merged_split_trio_df.alleles.apply(ast.literal_eval).apply(pd.Series).apply(':'.join, axis=1) + ':' + merged_split_trio_df.SAMPLE.astype(str)
 merged_split_trio_df.index = merged_split_trio_df.VarKey
 
-trio_denovo_df['VarKey'] = trio_denovo_df[['#CHROM', 'POS', 'REF', 'ALT']].astype(str).apply(':'.join, axis=1) + ':' + trio_denovo_df.SAMPLE.astype(str)
+trio_denovo_df['VarKey'] = trio_denovo_df[['CHROM', 'POS', 'REF', 'ALT']].astype(str).apply(':'.join, axis=1) + ':' + trio_denovo_df.SAMPLE.astype(str)
 trio_denovo_df.index = trio_denovo_df.VarKey
 
 merged_split_trio_df['in_step04'] = merged_split_trio_df.index.isin(trio_denovo_df.index)
