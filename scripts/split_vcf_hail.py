dbd6f310d43569d9c2acec27900e80ff16575e07
Eren Shin <eshin0525@gmail.com>
2024-03-18 12:56:54 -0400

    row_fields_to_keep comment out

--- scripts/split_vcf_hail.py
+++ scripts/split_vcf_hail.py
@@ -8,10 +8,10 @@ records_per_shard = int(sys.argv[3])
 prefix = sys.argv[4]
 cores = sys.argv[5]
 mem = int(np.floor(float(sys.argv[6])))
-row_fields_to_keep = sys.argv[7].split(',')
+# row_fields_to_keep = sys.argv[7].split(',')
 
-if row_fields_to_keep[0] == 'false':
-    row_fields_to_keep = []
+# if row_fields_to_keep[0] == 'false':
+#     row_fields_to_keep = []
 
 hl.init(min_block_size=128, spark_conf={"spark.executor.cores": cores, 
                     "spark.executor.memory": f"{mem}g",
@@ -45,7 +45,7 @@ if n_shards!=0:
     mt = mt.repartition(n_shards)
 
 # put all in INFO to be kept when exported to VCF--this doesn't really work because of header/metadata issues...
-for field in row_fields_to_keep:
-    mt = mt = mt.annotate_rows(info = mt.info.annotate(**{field: getattr(mt, field)}))
+# for field in row_fields_to_keep:
+#     mt = mt = mt.annotate_rows(info = mt.info.annotate(**{field: getattr(mt, field)}))
 
 hl.export_vcf(mt, output=prefix+'.vcf.bgz', parallel='header_per_shard', metadata=header if is_vcf else None)
