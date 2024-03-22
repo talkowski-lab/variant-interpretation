69d346223640f548ab87a19bffc5883811d570e4
Eren Shin <eshin0525@gmail.com>
2024-03-22 10:12:05 -0400

    predict sex in hail using Somalier's criteria

--- scripts/hail_impute_sex.py
+++ scripts/hail_impute_sex.py
@@ -43,28 +43,30 @@ mt = split_multi_ssc(mt)
 intervals = hl.import_bed(bed_file, reference_genome='GRCh38')
 mt = mt.filter_rows(hl.is_defined(intervals[mt.locus]))
 
-imputed_sex = hl.impute_sex(mt.GT)
-sex_df = imputed_sex.to_pandas()
-
-sex_df['sex'] = sex_df.is_female.map({False: 1, True: 2})
-sex_df.loc[sex_df.is_female.isna(), 'sex'] = 0
-
-sex_df.index = sex_df.s.astype(str)
-sex_df.index.name = 'sample_id'
+mt = mt.annotate_entries(AB=mt.AD[1]/hl.sum(mt.AD))
+mt = mt.annotate_entries(GT=hl.or_missing(mt.DP!=0, mt.GT))
+# only consider hets with 0.3 <= AB <= 0.7
+mt = mt.annotate_entries(GT=hl.if_else(mt.GT.is_het(),
+                         hl.or_missing((mt.AB>=0.3) & (mt.AB<=0.7), mt.GT), mt.GT))
 
 all_qc = hl.sample_qc(mt)
 all_qc_df = all_qc.cols().flatten().to_pandas()
 all_qc_df = all_qc_df.rename({'s': 'sample_id'}, axis=1).set_index('sample_id')
 
-gt_qc = hl.sample_qc(mt.filter_entries(hl.is_missing(mt.GT), keep=False))
+gt_mt = mt.filter_entries(hl.is_missing(mt.GT), keep=False)
+gt_mt = gt_mt.filter_entries(gt_mt.DP>=7)
+gt_qc = hl.sample_qc(gt_mt)
 gt_qc_df = gt_qc.cols().flatten().to_pandas()
 gt_qc_df = gt_qc_df.rename({'s': 'sample_id'}, axis=1).set_index('sample_id')
 
-sex_chrX_qc = hl.sample_qc(mt.filter_rows(mt.locus.in_x_nonpar()))
+x_mt = mt.filter_rows(mt.locus.in_x_nonpar())
+x_mt = x_mt.filter_entries(x_mt.DP>=7)
+sex_chrX_qc = hl.sample_qc(x_mt)
 sex_chrX_qc_df = sex_chrX_qc.cols().flatten().to_pandas()
 sex_chrX_qc_df = sex_chrX_qc_df.rename({'s': 'sample_id'}, axis=1).set_index('sample_id')
 
-sex_chrY_qc = hl.sample_qc(mt.filter_rows(mt.locus.in_y_nonpar()))
+y_mt = mt.filter_rows(mt.locus.in_y_nonpar())
+sex_chrY_qc = hl.sample_qc(y_mt)
 sex_chrY_qc_df = sex_chrY_qc.cols().flatten().to_pandas()
 sex_chrY_qc_df = sex_chrY_qc_df.rename({'s': 'sample_id'}, axis=1).set_index('sample_id')
 
@@ -74,29 +76,41 @@ all_qc_df.columns = all_qc_df.columns.str.replace('sample_qc', 'all')
 gt_qc_df.columns = gt_qc_df.columns.str.replace('sample_qc', 'gt')
 
 sample_qc_df = pd.concat([sex_chrX_qc_df, sex_chrY_qc_df, all_qc_df, gt_qc_df], axis=1)
-sample_qc_df['hail_sex'] = sex_df.sex.astype('category')
 
 sample_qc_df['Y_ploidy'] = 2 * sample_qc_df['chrY.dp_stats.mean'] / sample_qc_df['gt.dp_stats.mean']
 sample_qc_df['X_ploidy'] = 2 * sample_qc_df['chrX.dp_stats.mean'] / sample_qc_df['gt.dp_stats.mean']
 
-# sample_qc_df = sample_qc_df.loc[:,~sample_qc_df.columns.duplicated()].copy()
-
-fig, ax = plt.subplots(2, 2, figsize=(12, 10));
-sns.scatterplot(data=sample_qc_df, x='chrX.n_hom_var', y='chrX.n_het', hue='hail_sex', ax=ax[0][0]);
-sns.scatterplot(data=sample_qc_df, x='chrX.n_hom_var', y='chrY.dp_stats.mean', hue='hail_sex', ax=ax[0][1]);
-sns.scatterplot(data=sample_qc_df, x='chrX.dp_stats.mean', y='chrY.dp_stats.mean', hue='hail_sex', ax=ax[1][0]);
-sns.scatterplot(data=sample_qc_df, x='X_ploidy', y='Y_ploidy', hue='hail_sex', ax=ax[1][1]);
-plt.savefig(f"{cohort_prefix}_sex_qc_imputed.png");
+# Somalier's sex prediction criteria
+def predict_sex(row):
+    sex = -9
+    if (row['chrX.n_het'] / row['chrX.n_hom_var'] < 0.05) & (row['chrX.n_called'] > 10):
+        sex = 1
+    elif (row['chrX.n_het'] / row['chrX.n_hom_var'] > 0.4) & (row['chrX.n_called'] > 10):
+        sex = 2
+    if (row['chrY.n_called']>0) & (row.sex==1) & (row['Y_ploidy'] < 0.4):
+        sex = -1  # apparent loss of Y with low het-ratio on X chromosome
+    if (row['chrY.n_called']>0) & (row.sex==2) & (row['Y_ploidy'] > 0.4):
+        sex = -2  # apparent Y with high het-ratio on X chromosome
+    return sex
 
 ped = pd.read_csv(ped_uri, sep='\t').iloc[:, :6]
 base_cols = ['family_id', 'sample_id', 'paternal_id', 'maternal_id', 'sex', 'phenotype']
 ped.columns = base_cols
 ped = ped.set_index('sample_id')
 
-ped_qc = pd.concat([ped, sample_qc_df], axis=1).reset_index()[base_cols + np.setdiff1d(sample_qc_df.columns, base_cols).tolist()]
+ped_qc = pd.concat([ped, sample_qc_df], axis=1).reset_index()
+ped_qc['pred_sex'] = ped_qc.apply(predict_sex, axis=1).astype('category')
+ped_qc = ped_qc[base_cols + ['pred_sex'] + np.setdiff1d(sample_qc_df.columns, base_cols).tolist()]
 ped_qc.to_csv(f"{cohort_prefix}_sex_qc.ped", sep='\t', index=False)
 
 ped_qc['sex'] = ped_qc.sex.astype('category')
+
+fig, ax = plt.subplots(2, 2, figsize=(12, 10));
+sns.scatterplot(data=sample_qc_df, x='chrX.n_hom_var', y='chrX.n_het', hue='pred_sex', ax=ax[0][0]);
+sns.scatterplot(data=sample_qc_df, x='chrX.n_hom_var', y='chrY.dp_stats.mean', hue='pred_sex', ax=ax[0][1]);
+sns.scatterplot(data=sample_qc_df, x='chrX.dp_stats.mean', y='chrY.dp_stats.mean', hue='pred_sex', ax=ax[1][0]);
+sns.scatterplot(data=sample_qc_df, x='X_ploidy', y='Y_ploidy', hue='pred_sex', ax=ax[1][1]);
+plt.savefig(f"{cohort_prefix}_sex_qc_imputed.png");
 
 fig, ax = plt.subplots(2, 2, figsize=(12, 10));
 sns.scatterplot(data=ped_qc, x='chrX.n_hom_var', y='chrX.n_het', hue='sex', ax=ax[0][0]);
