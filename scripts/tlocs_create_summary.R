0343b57d94a961859cd50cc2538cbab9a9a0f8fd
alsanju <alba.sanchis91@gmail.com>
2024-03-25 12:21:25 -0400

    Update summary

--- scripts/tlocs_create_summary.R
+++ scripts/tlocs_create_summary.R
@@ -42,13 +42,46 @@ summary_df <- do.call(rbind, apply(samples, 1, function(row){
   BP2_int_plus <- BP2_1_plus-BP2_2_plus
   BP2_int_minus <- BP2_1_minus-BP2_2_minus
 
+  First_PE_dir <- info[order(info$V2),]$V3[1]
+  PE <- nrow(info)
+  Count1 <- nrow(subset(info, V3 == "+" & V6 == "+"))
+  Count2 <- nrow(subset(info, V3 == "+" & V6 == "-"))
+  Count3 <- nrow(subset(info, V3 == "-" & V6 == "+"))
+  Count4 <- nrow(subset(info, V3 == "-" & V6 == "-"))
+
+  if(info[order(info$V2),]$V3[1] == "-"){
+    count <- 0
+    vector <- info[order(info$V2),]$V3
+    for (i in 1:(length(vector) - 1)) {
+      if (vector[i] == "-" && vector[i + 1] == "-") {
+        count <- count + 1
+      } else if (vector[i] == "+") {
+        count <- count + 1
+        break  # Interrupt loop if "+" is encountered
+      }
+    }
+    # Output the result
+    Count_ins_PE <- count
+
+  }else{
+    Count_ins_PE <- 0
+  }
+
   return(data.frame(BP1_1_plus, BP1_2_plus, BP1_1_minus, BP1_2_minus, BP1_int_plus, BP1_int_minus,
-                    BP2_1_plus, BP2_2_plus, BP2_1_minus, BP2_2_minus, BP2_int_plus, BP2_int_minus))
+                    BP2_1_plus, BP2_2_plus, BP2_1_minus, BP2_2_minus, BP2_int_plus, BP2_int_minus,
+                    First_PE_dir, PE, Count1, Count2, Count3, Count4, Count_ins_PE))
 
 }))
 
 #Append to main samples table
 samples_summary <- cbind(samples, summary_df)
+
+samples_summary$pass1 <- lapply(1:nrow(samples_summary), function(i) if ((as.numeric(samples_summary$BP1_int_plus[i])*-1) > 50) {"1"} else {"0"})
+samples_summary$pass2 <- lapply(1:nrow(samples_summary), function(i) if ((as.numeric(samples_summary$BP1_int_minus[i])*-1) > 50) {"1"} else {"0"})
+samples_summary$pass3 <- lapply(1:nrow(samples_summary), function(i) if ((as.numeric(samples_summary$BP2_int_plus[i])*-1) > 50) {"1"} else {"0"})
+samples_summary$pass4 <- lapply(1:nrow(samples_summary), function(i) if ((as.numeric(samples_summary$BP2_int_minus[i])*-1) > 50) {"1"} else {"0"})
+samples_summary$Total_pass<-  lapply(1:nrow(samples_summary), function(i) ( sum(as.numeric(unlist(samples_summary$pass1[i])), as.numeric(unlist(samples_summary$pass2[i])),
+                                                                                as.numeric(unlist(samples_summary$pass3[i])), as.numeric(unlist(samples_summary$pass4[i])))))
 
 #Write output
 write.table(samples_summary, out, sep = "\t", quote = F, row.names = F, col.names = F)
