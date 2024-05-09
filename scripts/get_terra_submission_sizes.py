e3fd86497f7d4a1697fda7188935111ea00c96a1
Eren Shin <eshin0525@gmail.com>
2024-05-09 14:37:07 -0400

    getSubmissionSizes

---
+++ scripts/get_terra_submission_sizes.py
@@ -0,0 +1,172 @@
+from typing import Union
+from tenacity import retry, after_log, before_sleep_log, retry_if_exception_type, stop_after_attempt, wait_exponential
+from firecloud import fiss
+from firecloud.errors import FireCloudServerError
+import firecloud.api as fapi
+import pandas as pd 
+import numpy as np
+import re
+import sys
+import time
+import math
+import ast
+import os
+import datetime
+
+import logging
+from logging import INFO, DEBUG
+logger = logging.getLogger()
+logger.setLevel(INFO)
+
+from google.cloud import storage
+import pandas as pd 
+import numpy as np
+import datetime
+import sys
+from terra_data_table_util import get_terra_table_to_df
+
+BILLING_PROJECT_ID = sys.argv[1]
+WORKSPACE = sys.argv[2]
+BUCKET = sys.argv[3]
+
+bucket_id = BUCKET.split('gs://')[1]
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucket_id)
+
+cohort_df = get_terra_table_to_df(project=BILLING_PROJECT_ID, workspace=WORKSPACE, table_name='cohort')
+cohort_df.index = cohort_df['entity:cohort_id']
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def humansize(nbytes):
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+submissions = pd.DataFrame(fapi.list_submissions(BILLING_PROJECT_ID, WORKSPACE).json()).sort_values('submissionDate',ascending=False).set_index('submissionId',drop=False)
+submissions = pd.concat([submissions, submissions.submissionEntity.apply(pd.Series)], axis=1)
+submissions['submissionDate'] = submissions.submissionDate.apply(lambda date_str: datetime.datetime.fromisoformat(date_str[:-1]))
+
+# Get output names for each method/task
+submissions['methodConfigurationNameClean'] = submissions.methodConfigurationName.str.split('_').str[0]
+method_outputs = {}
+for x in submissions.methodConfigurationNameClean.unique():
+    try:
+        method_outputs[x] = [x.split('.')[1] for x in fapi.get_workspace_config(config=x, 
+                          namespace=BILLING_PROJECT_ID, 
+                          cnamespace=BILLING_PROJECT_ID, workspace=WORKSPACE).json()['outputs'].values()]
+    except:
+        method_outputs[x] = np.nan
+
+only_successful = submissions[submissions.workflowStatuses.apply(lambda dct: 'Succeeded' in list(dct.keys()))]
+only_failed = submissions[~submissions.index.isin(only_successful.index)]
+
+# Successful submissions first
+tot_subs = only_successful.shape[0]
+i = 0
+successful_submissions = pd.DataFrame()
+
+# Get workflow info
+print("------ Getting workflow info for each submission ------")
+for sub_id, submission in only_successful.iterrows():
+    i += 1
+    if (i%10==0): 
+        print(f"submission {i}/{tot_subs}...")
+    metadata = pd.DataFrame(fapi.get_submission(submission_id=sub_id, namespace=BILLING_PROJECT_ID, workspace=WORKSPACE).json()['workflows'])
+    try:
+        submission['workflowId'] = metadata['workflowId'].tolist()
+        submission['workflowStatus'] = metadata['status'].tolist()
+        submission['workflowEntity'] = metadata['workflowEntity'].tolist()
+    except:
+        pass
+    successful_submissions = pd.concat([successful_submissions, pd.DataFrame(submission).T])
+
+# Get cohort sets into separate rows by cohort
+successful_submissions_exploded = successful_submissions.explode(['workflowId','workflowStatus','workflowEntity'])
+# update entityName and entityType
+successful_submissions_exploded = successful_submissions_exploded.workflowEntity.apply(pd.Series).combine_first(successful_submissions_exploded)
+
+successful_submissions_exploded['method_outputs'] = successful_submissions_exploded.methodConfigurationNameClean.map(method_outputs)
+
+# Get actual outputs for each submission and workflow
+print("------ Getting workflow outputs for each submission ------")
+successful_submission_outputs = pd.DataFrame()
+tot_subs = successful_submissions_exploded.shape[0]
+i = 0
+for sub_id, submission in successful_submissions_exploded.iterrows():
+    i += 1
+    if (i%10==0): 
+        print(f"submission {i}/{tot_subs}...")
+    outputs = {k.split('.')[1]: v for k, v in fapi.get_workflow_metadata(namespace=BILLING_PROJECT_ID, workspace=WORKSPACE, submission_id=sub_id, workflow_id=submission.workflowId).json()['outputs'].items()}
+    submission['method_outputs'] = outputs.keys()
+    submission['workflow_outputs'] = outputs.values()
+    successful_submission_outputs = pd.concat([successful_submission_outputs, pd.DataFrame(submission).T])    
+
+# Get multiple outputs for a method into separate rows
+successful_submission_outputs = successful_submission_outputs.explode(['method_outputs', 'workflow_outputs'])
+
+# Check if in Terra data table
+def in_data_table(submission):
+    cohort = submission.entityName
+    output_name = submission.method_outputs
+    try:
+        workflow_output = submission.workflow_outputs
+        if '[' in workflow_output:
+            workflow_output = ast.literal_eval(workflow_output)
+        data_table_val = cohort_df.loc[cohort, output_name]
+        if '[' in data_table_val:
+            data_table_val = ast.literal_eval(data_table_val)
+        return data_table_val==workflow_output
+    except Exception as e:
+        return np.nan
+
+successful_submission_outputs['in_data_table'] = successful_submission_outputs.apply(in_data_table, axis=1)
+
+# Sanity check that we're keeping one of each output for each cohort
+entity_outputs_to_remove = successful_submission_outputs[successful_submission_outputs.in_data_table==False][['entityName','method_outputs']].value_counts().reset_index()
+entity_outputs_to_keep = successful_submission_outputs[successful_submission_outputs.in_data_table==True][['entityName','method_outputs']].value_counts().reset_index()
+
+entity_outputs_to_remove['entityName:method_outputs'] = entity_outputs_to_remove[['entityName','method_outputs']].agg(':'.join, axis=1)
+entity_outputs_to_keep['entityName:method_outputs'] = entity_outputs_to_keep[['entityName','method_outputs']].agg(':'.join, axis=1)
+
+print("--- Check that all entities and method outputs being removed have an updated version ---")
+entity_outputs_to_remove['entityName:method_outputs'].isin(entity_outputs_to_keep['entityName:method_outputs']).value_counts()
+
+n_filled_in_data_table = (~cohort_df.isna()).sum()[np.intersect1d(cohort_df.columns, successful_submission_outputs.method_outputs.dropna().unique())].sum()
+print(f"Terra data table has {n_filled_in_data_table} total entries filled.")
+
+# Get sizes
+def get_blob_size_directory(uri):
+    tot_size = 0
+    blobs_list = storage_client.list_blobs(bucket, prefix=uri.split(BUCKET)[1][1:])
+    for blob in blobs_list:
+        tot_size += blob.size
+    return tot_size
+
+def get_blob_size(workflow_outputs):
+    try:
+        if type(workflow_outputs)!=list:
+            workflow_outputs = [workflow_outputs]
+        tot_size = 0
+        for uri in workflow_outputs:
+            if '.mt' in uri or '.ht' in uri:
+                tot_size += get_blob_size_directory(uri)
+            else:
+                blob = bucket.get_blob(uri.split(BUCKET)[1][1:])
+                tot_size += blob.size
+        return tot_size
+    except Exception as e:
+        return np.nan
+    
+print("--- Getting file sizes for successful submissions' outputs ---")
+successful_submission_outputs['sizes'] = successful_submission_outputs.workflow_outputs.apply(get_blob_size)
+
+# Now get sizes for failed submission directories
+print("--- Getting file sizes for failed submissions' outputs ---")
+only_failed['uri'] = BUCKET + '/submissions/' + only_failed.submissionId + '/'
+only_failed['sizes'] = only_failed.uri.apply(get_blob_size_directory)
+
+successful_submission_outputs.to_csv(f"{str(datetime.datetime.now().strftime('%Y-%m-%d'))}_successful_submissions.tsv", sep='\t', index=False)
+only_failed.to_csv(f"{str(datetime.datetime.now().strftime('%Y-%m-%d'))}_failed_submissions.tsv", sep='\t', index=False)
---
+++ wdl/getTerraSubmissionSizes.wdl
@@ -0,0 +1,93 @@
+version 1.0
+
+struct RuntimeAttr {
+    Float? mem_gb
+    Int? cpu_cores
+    Int? disk_gb
+    Int? boot_disk_gb
+    Int? preemptible_tries
+    Int? max_retries
+}
+
+workflow getTerraSubmissionSizes {
+    input {
+        String BILLING_PROJECT_ID
+        String WORKSPACE
+        String bucket_id
+        String hail_docker
+        String terra_data_table_util_script
+        String get_submission_sizes_script
+        Boolean delete_submissions=false
+    }
+
+    call getSubmissionSizes {
+        input:
+        BILLING_PROJECT_ID=BILLING_PROJECT_ID,
+        WORKSPACE=WORKSPACE,
+        bucket_id=bucket_id,
+        hail_docker=hail_docker,
+        terra_data_table_util_script=terra_data_table_util_script,
+        get_submission_sizes_script=get_submission_sizes_script
+    }
+
+    # if (delete_submissions) {
+    #     scatter (submissions_shard in getSubmissionsToDelete.submissions_to_delete) {
+    #         call deleteSubmissions {
+    #             input:
+    #             submissions_to_delete=submissions_shard,
+    #             hail_docker=hail_docker
+    #         }
+    #     }
+    # }
+    output {
+        File successful_submissions = getSubmissionSizes.successful_submissions
+        File failed_submissions = getSubmissionSizes.failed_submissions
+        # Array[File] submissions_to_delete = getSubmissionsToDelete.submissions_to_delete
+        # String space_cleared = getSubmissionsToDelete.space_cleared
+    }
+}
+
+task getSubmissionSizes {
+    input {
+        String BILLING_PROJECT_ID
+        String WORKSPACE
+        String bucket_id
+        String hail_docker
+        String terra_data_table_util_script
+        String get_submission_sizes_script
+        RuntimeAttr? runtime_attr_override
+    }
+    Float base_disk_gb = 10.0
+
+    RuntimeAttr runtime_default = object {
+        mem_gb: 4,
+        disk_gb: ceil(base_disk_gb),
+        cpu_cores: 1,
+        preemptible_tries: 3,
+        max_retries: 1,
+        boot_disk_gb: 10
+    }
+
+    RuntimeAttr runtime_override = select_first([runtime_attr_override, runtime_default])
+
+    runtime {
+        memory: "~{select_first([runtime_override.mem_gb, runtime_default.mem_gb])} GB"
+        disks: "local-disk ~{select_first([runtime_override.disk_gb, runtime_default.disk_gb])} HDD"
+        cpu: select_first([runtime_override.cpu_cores, runtime_default.cpu_cores])
+        preemptible: select_first([runtime_override.preemptible_tries, runtime_default.preemptible_tries])
+        maxRetries: select_first([runtime_override.max_retries, runtime_default.max_retries])
+        docker: hail_docker
+        bootDiskSizeGb: select_first([runtime_override.boot_disk_gb, runtime_default.boot_disk_gb])
+    }
+    
+    command <<<
+        curl ~{terra_data_table_util_script} > terra_data_table_util.py
+        curl ~{get_submission_sizes_script} > get_submission_sizes.py
+        python3 get_submission_sizes.py ~{BILLING_PROJECT_ID} ~{WORKSPACE} ~[bucket_id] > stdout
+    >>>
+
+    output {
+        File successful_submissions = glob('_successful_submissions.tsv')[0]
+        File failed_submissions = glob('_failed_submissions.tsv')[0]
+    }
+}
