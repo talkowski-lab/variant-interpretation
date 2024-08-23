2f9cf32558917ccc66f713d7b114037d6cec5bd0
Eren Shin <eshin0525@gmail.com>
2024-08-22 20:13:40 -0400

    trio_samples in VCF

--- scripts/hail_filter_comphets_xlr_hom_var.py
+++ scripts/hail_filter_comphets_xlr_hom_var.py
@@ -254,7 +254,11 @@ def get_transmission(phased_tm_ht):
 tmp_ped = pd.read_csv(ped_uri, sep='\t').iloc[:,:6]
 tmp_ped.to_csv(f"{prefix}.ped", sep='\t', index=False)
 pedigree = hl.Pedigree.read(f"{prefix}.ped", delimiter='\t')
-trio_samples = list(np.array([[trio.s, trio.pat_id, trio.mat_id] for trio in pedigree.complete_trios() if trio.fam_id!='-9']).flatten())
+
+vcf_samples = merged_mt.s.collect()
+trio_samples = list(np.intersect1d(vcf_samples,
+                              list(np.array([[trio.s, trio.pat_id, trio.mat_id] 
+                                             for trio in pedigree.complete_trios() if trio.fam_id!='-9']).flatten())))
 
 # Get CompHets
 merged_trio_comphets = get_trio_comphets(merged_mt)
