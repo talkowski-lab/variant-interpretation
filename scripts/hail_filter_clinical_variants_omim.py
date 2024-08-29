db109f94881b45fdfd5ff9921808ad3bf2fed4dc
Eren Shin <eshin0525@gmail.com>
2024-08-29 18:31:24 -0400

    edited OMIM dominant/recessive logic

--- scripts/hail_filter_clinical_variants_omim.py
+++ scripts/hail_filter_clinical_variants_omim.py
@@ -112,10 +112,12 @@ omim_rec_gene_phased_tm = gene_phased_tm.filter_rows(
     (gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code.matches('2')) |    # OMIM recessive
     (gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code.matches('4')) |  # XLR
     ((hl.is_missing(gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code)) &  # not OMIM recessive with gnomAD AF and MPC filters
-                            ((gene_phased_tm.gnomad_popmax_af<=gnomad_rec_threshold) | (hl.is_missing(gene_phased_tm.gnomad_popmax_af))) &
-                            ((gene_phased_tm.info.MPC>=mpc_threshold) | (hl.is_missing(gene_phased_tm.info.MPC))) &
-                            (hl.if_else(gene_phased_tm.vep.transcript_consequences.am_pathogenicity=='', 1, 
-                               hl.float(gene_phased_tm.vep.transcript_consequences.am_pathogenicity))>=am_threshold)
+        ((gene_phased_tm.gnomad_popmax_af<=gnomad_rec_threshold) | (hl.is_missing(gene_phased_tm.gnomad_popmax_af)) &
+            (((gene_phased_tm.info.MPC>=mpc_threshold) | (hl.is_missing(gene_phased_tm.info.MPC))) |
+            (hl.if_else(gene_phased_tm.vep.transcript_consequences.am_pathogenicity=='', 1, 
+                hl.float(gene_phased_tm.vep.transcript_consequences.am_pathogenicity))>=am_threshold)
+            )
+        )
     )
 )
 
@@ -142,16 +144,15 @@ gene_phased_tm = gene_phased_tm.annotate_rows(vep=gene_phased_tm.vep.annotate(tr
 omim_dom = gene_phased_tm.filter_rows(
     ((gene_phased_tm.gnomad_popmax_af<=gnomad_dom_threshold) | (hl.is_missing(gene_phased_tm.gnomad_popmax_af))) & 
         ((gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code.matches('1')) |   # OMIM dominant with gnomAD AF filter
-        ((hl.is_missing(gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code)) &  # not OMIM dominant with MPC filter OR (AM filter AND LOEUF v2/v4 filters)
-            (((gene_phased_tm.info.MPC>=mpc_threshold) | (hl.is_missing(gene_phased_tm.info.MPC))) |
-            ((hl.if_else(gene_phased_tm.vep.transcript_consequences.am_pathogenicity=='', 1, 
-            hl.float(gene_phased_tm.vep.transcript_consequences.am_pathogenicity))>=am_threshold) &
+        ((hl.is_missing(gene_phased_tm.vep.transcript_consequences.OMIM_inheritance_code)) &  # not OMIM dominant with (MPC filter OR AM filter) AND LOEUF v2/v4 filters)
+            ((((gene_phased_tm.info.MPC>=mpc_threshold) | (hl.is_missing(gene_phased_tm.info.MPC))) |
+            (hl.if_else(gene_phased_tm.vep.transcript_consequences.am_pathogenicity=='', 1, 
+            hl.float(gene_phased_tm.vep.transcript_consequences.am_pathogenicity))>=am_threshold)) &
                 ((hl.if_else(gene_phased_tm.vep.transcript_consequences.LOEUF_v2=='', 0, 
                     hl.float(gene_phased_tm.vep.transcript_consequences.LOEUF_v2))<=loeuf_v2_threshold) | 
                 (hl.if_else(gene_phased_tm.vep.transcript_consequences.LOEUF_v4=='', 0, 
                     hl.float(gene_phased_tm.vep.transcript_consequences.LOEUF_v4))<=loeuf_v4_threshold)
                 )
-            )
             )
         )
         )
